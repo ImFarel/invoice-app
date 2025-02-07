cc29c385d4ee895dd67bb8de4be13c553142ea59
imfarel <sayafarel@gmail.com>
2025-02-07 13:05:27 +0700

    init

---
+++ src/components/layout/horizontal/FooterContent.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+// Next Imports
+import Link from 'next/link'
+
+// Third-party Imports
+import classnames from 'classnames'
+
+
+// Util Imports
+import { horizontalLayoutClasses } from '@layouts/utils/layoutClasses'
+
+const FooterContent = () => {
+  // Hooks
+
+  return (
+    <div
+      className={classnames(horizontalLayoutClasses.footerContent, 'flex items-center justify-between flex-wrap gap-4')}
+    >
+      <p>
+        <span className='text-textSecondary'>{`© ${new Date().getFullYear()}, Made with `}</span>
+        <span>{`❤️`}</span>
+        <span className='text-textSecondary'>{` by `}</span>
+        <Link href='https://pixinvent.com/' target='_blank' className='text-primary uppercase'>
+          Pixinvent
+        </Link>
+      </p>
+
+    </div>
+  )
+}
+
+export default FooterContent
